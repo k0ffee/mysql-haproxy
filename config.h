173ca3493fb8f7640de12b35f5a31cbb652c2dc8
Tino Reinhardt <t@sdf.org>
2018-03-21 21:23:54 +0100

    Whitespace cosmetics

--- config.h
+++ config.h
@@ -7,22 +7,20 @@
 #define MYSQL_USER          "haproxy"
 #define MYSQL_PASSWORD      ""
 
-#ifdef __APPLE__
-#define MSG_NOSIGNAL 0x0  /* send(2) */
-#endif
-
 #ifdef __MYSQL__
-    #define GOOD_IO_RUNNING     "Yes" /* I/O thread */
-    #define GOOD_SQL_RUNNING    "Yes" /* SQL thread */
-    #define GOOD_SECONDS_BEHIND 4     /* maximum acceptable lag behind master */
-
-    #define QUERY               "show slave status"
+#define GOOD_IO_RUNNING     "Yes" /* I/O thread */
+#define GOOD_SQL_RUNNING    "Yes" /* SQL thread */
+#define GOOD_SECONDS_BEHIND 4     /* maximum acceptable lag behind master */
+#define QUERY               "show slave status"
 
 #elif __GALERA__
-    #define GOOD_GALERA_STATUS  "4"   /* wsrep_local_state */
-    #define READ_ONLY_STATUS    "OFF"
+#define GOOD_GALERA_STATUS  "4"   /* wsrep_local_state */
+#define READ_ONLY_STATUS    "OFF"
+#define QUERY_WSREP_STATE   "show global status where " \
+                            "variable_name='wsrep_local_state'"
+#define QUERY_READ_ONLY     "show global variables like 'read_only'"
+#endif
 
-    #define QUERY_WSREP_STATE   "show global status where " \
-                                "variable_name='wsrep_local_state'"
-    #define QUERY_READ_ONLY     "show global variables like 'read_only'"
+#ifdef __APPLE__
+#define MSG_NOSIGNAL 0x0    /* send(2) */
 #endif
